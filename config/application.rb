db4c4c8c6130d2ce61c524b141e9646f656a8fe6
Loic <lginoux@gmail.com>
2012-05-18 10:06:40 +0200

    Correct bug user session form view

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,11 @@ module Foodrubix
     #email configuration
     config.action_mailer.delivery_method = :smtp
     
+    #because of bug on heroku about precompliling assets
+    #https://devcenter.heroku.com/articles/rails3x-asset-pipeline-cedar#troubleshooting
+    config.assets.initialize_on_precompile = false
+    
+    
     
   end
 end
