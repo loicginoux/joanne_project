802f3e260e9247dc28582176dc599e5039d73a75
Loic <lginoux@gmail.com>
2012-07-12 13:08:03 +0200

    add facebook details

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,9 @@
+facebook_config_file = File.join(Rails.root,'config','facebook.yml')
+raise "#{facebook_config_file} is missing!" unless File.exists? facebook_config_file
+FB_CONFIG = YAML.load_file(facebook_config_file)[Rails.env].symbolize_keys
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '445409062150241', '997dc180be8d351ff126d4a97009c26e' ,
+  provider :facebook, FB_CONFIG[:fb_app_id], FB_CONFIG[:secret_access_key] ,
           :scope => %(email, publish_stream)
   provider :twitter, 'NzRDtueA50fYl7HxWv00Ow', 'pdiJqwaT1awEhVTehuJ7R9qJKXP92AcuuQlUnVlEg'
 end
