5eacb9ddddeccd07ef8f8fbee1e0d11dfff56f64
Loic <lginoux@gmail.com>
2012-09-18 11:56:29 +0200

    remove require_login for data point creation

--- app/controllers/data_points_controller.rb
+++ app/controllers/data_points_controller.rb
@@ -1,6 +1,6 @@
 class DataPointsController < ApplicationController
   before_filter :check_for_cancel, :only => [:create, :update]
-  before_filter :require_login
+  before_filter :require_login, :except => [:create]
   
   def index
     if(params.has_key?(:start_date) && params.has_key?(:end_date)) && params.has_key?(:user_id)
