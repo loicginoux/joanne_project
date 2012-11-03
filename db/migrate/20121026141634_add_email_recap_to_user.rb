d70fc9ccb7c08366bc7925aa8e37cb2f04ca40fd
Loic <lginoux@gmail.com>
2012-11-03 10:17:15 +0100

    change default email recap

--- db/migrate/20121026141634_add_email_recap_to_user.rb
+++ db/migrate/20121026141634_add_email_recap_to_user.rb
@@ -1,6 +1,6 @@
 class AddEmailRecapToUser < ActiveRecord::Migration
 	def change
-		add_column :users, :daily_email, :boolean, :default => true
-		add_column :users, :weekly_email, :boolean, :default => true
+		add_column :users, :daily_email, :boolean, :default => 1
+		add_column :users, :weekly_email, :boolean, :default => 1
 	end
 end
