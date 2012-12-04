45b6a47d72330bd63096cd7afcbd990bdb8543b3
Loic <lginoux@gmail.com>
2012-12-04 12:56:41 +0000

    remove email sending to  admins

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -7,11 +7,4 @@ class AdminUser < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-
-  after_create { |admin| admin.send_reset_password_instructions }
-
-  def password_required?
-    new_record? ? false : super
-  end
-
 end
