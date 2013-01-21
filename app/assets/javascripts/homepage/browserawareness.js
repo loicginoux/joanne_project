f2ad356f831ee136c4af90aa5283a0868de4dbf8
Loic <lginoux@gmail.com>
2013-01-21 18:18:07 +0000

    add browser awarness

---
+++ app/assets/javascripts/homepage/browserawareness.js
@@ -0,0 +1,6 @@
+  (function(d, t) {
+    var e = d.createElement(t);
+    e.src = d.location.protocol + '//www.browserawarenessday.com/widget/50fd84d4e42412ac4500006e';
+    e.async = true;
+    var n = d.getElementsByTagName(t)[0]; n.parentNode.insertBefore(e, n);
+  })(document, 'script');
