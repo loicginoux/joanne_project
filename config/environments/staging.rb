80460a035a65184b8ba44a842243b683f6b14d11
Loic <lginoux@gmail.com>
2013-02-17 16:15:55 +0000

    test staging.rb

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -12,7 +12,7 @@ Foodrubix::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = false
+  config.assets.compress = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
