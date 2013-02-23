ad4a7575478b81bfcce6c3783cea9643a4bb6c81
Loic <lginoux@gmail.com>
2013-02-23 14:52:49 +0100

    gzip

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -15,7 +15,7 @@ Foodrubix::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # caching
   config.cache_store = :dalli_store
