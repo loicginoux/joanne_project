b0cf197a2073e9a3071e356c1d0f3624a9105cfb
Loic <lginoux@gmail.com>
2013-03-18 17:43:13 +0100

    fix data point sweeper

--- app/sweepers/data_point_sweeper.rb
+++ app/sweepers/data_point_sweeper.rb
@@ -11,18 +11,21 @@ class DataPointSweeper < ActionController::Caching::Sweeper
 
   private
 
+
   def expire_cache(dp)
     weekNb = dp.uploaded_at.strftime("%U")
     monthNb = dp.uploaded_at.strftime("%m")
-    weekNbWas = dp.uploaded_at_was.strftime("%U")
-    monthNbWas = dp.uploaded_at_was.strftime("%m")
     Rails.cache.delete("/user/#{dp.user_id}/data_points/month/#{monthNb}")
     Rails.cache.delete("/user/#{dp.user_id}/data_points/month/#{monthNb}/graphicPoints")
     Rails.cache.delete("/user/#{dp.user_id}/data_points/week/#{weekNb}")
     Rails.cache.delete("/user/#{dp.user_id}/data_points/week/#{weekNb}/graphicPoints")
-    Rails.cache.delete("/user/#{dp.user_id}/data_points/month/#{monthNbWas}") unless monthNb == monthNbWas
-    Rails.cache.delete("/user/#{dp.user_id}/data_points/month/#{monthNbWas}/graphicPoints") unless monthNb == monthNbWas
-    Rails.cache.delete("/user/#{dp.user_id}/data_points/week/#{weekNbWas}") unless weekNb == weekNbWas
-    Rails.cache.delete("/user/#{dp.user_id}/data_points/week/#{weekNbWas}/graphicPoints") unless weekNb == weekNbWas
+    unless dp.uploaded_at_was.nil?
+      weekNbWas = dp.uploaded_at_was.strftime("%U")
+      monthNbWas = dp.uploaded_at_was.strftime("%m")
+      Rails.cache.delete("/user/#{dp.user_id}/data_points/month/#{monthNbWas}") unless monthNb == monthNbWas
+      Rails.cache.delete("/user/#{dp.user_id}/data_points/month/#{monthNbWas}/graphicPoints") unless monthNb == monthNbWas
+      Rails.cache.delete("/user/#{dp.user_id}/data_points/week/#{weekNbWas}") unless weekNb == weekNbWas
+      Rails.cache.delete("/user/#{dp.user_id}/data_points/week/#{weekNbWas}/graphicPoints") unless weekNb == weekNbWas
+    end
   end
 end
