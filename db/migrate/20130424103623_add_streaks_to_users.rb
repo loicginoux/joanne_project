9adfd6ed14b85d4597d449ba960c81c03a3740b7
Loic <lginoux@gmail.com>
2013-04-24 12:47:58 +0200

    update migration file

--- db/migrate/20130424103623_add_streaks_to_users.rb
+++ db/migrate/20130424103623_add_streaks_to_users.rb
@@ -1,7 +1,7 @@
 class AddStreaksToUsers < ActiveRecord::Migration
-  def change
-  	add_column :users, :best_daily_score,:default => 0
-    add_column :users,  :streak, :default => 0
-    add_column :users,  :best_streak, :default => 0
+  def up
+  	add_column :users, :best_daily_score, :integer, :default => 0
+    add_column :users,  :streak, :integer, :default => 0
+    add_column :users,  :best_streak, :integer, :default => 0
   end
 end
